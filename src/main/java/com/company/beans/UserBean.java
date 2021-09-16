3d54cb760752ea9c054762ca91096a9fea06e89c
abdalrazag <abdalrazag.alshrufat@gmail.com>
2021-09-16 23:38:20 +0300

    validate length of inputs

--- src/main/java/com/company/beans/UserBean.java
+++ src/main/java/com/company/beans/UserBean.java
@@ -12,6 +12,8 @@ public class UserBean {
 
     private String firstName;
     private String lastName;
+    private String email;
+    private String password;
     private String level;
     private String city;
     private Set<String> favorites;
@@ -68,5 +70,21 @@ public class UserBean {
 
     public void setFavorites(Set<String> favorites) {
         this.favorites = favorites;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
